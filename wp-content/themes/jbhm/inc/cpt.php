5eac2cd99f5cbce0c5b969e05972e67af48177f2
Will Hawks <will@creativedistillery.com>
2017-11-08 14:16:34 -0600

    Add taxonomies

--- wp-content/themes/jbhm/inc/cpt.php
+++ wp-content/themes/jbhm/inc/cpt.php
@@ -103,4 +103,36 @@
 
 	add_action( 'init', 'cptui_register_my_cpts' );
 
+	function cptui_register_my_taxes() {
+
+		/**
+		 * Taxonomy: Industries.
+		 */
+
+		$labels = array(
+			"name" => __( "Industries", "jbhm" ),
+			"singular_name" => __( "Industry", "jbhm" ),
+		);
+
+		$args = array(
+			"label" => __( "Industries", "jbhm" ),
+			"labels" => $labels,
+			"public" => true,
+			"hierarchical" => false,
+			"label" => "Industries",
+			"show_ui" => true,
+			"show_in_menu" => true,
+			"show_in_nav_menus" => true,
+			"query_var" => true,
+			"rewrite" => array( 'slug' => 'industries', 'with_front' => true, ),
+			"show_admin_column" => false,
+			"show_in_rest" => false,
+			"rest_base" => "",
+			"show_in_quick_edit" => false,
+		);
+		register_taxonomy( "industries", array( "project" ), $args );
+	}
+
+	add_action( 'init', 'cptui_register_my_taxes' );
+
 ?>
